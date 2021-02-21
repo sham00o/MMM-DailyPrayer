75ba404cb41bf74a661bd7b0e3b1269ea0f04c2f
Samuel Liu <lius94@gmail.com>
2021-02-21 16:46:11 -0700

    Add full body

--- node_helper.js
+++ node_helper.js
@@ -28,17 +28,23 @@ module.exports = NodeHelper.create({
 		request({ url: url, method: 'GET' }, function(error, response, body) {
 			if(!error && response.statusCode == 200){
 				var soup = new JSSoup(body);
+				// extract title
 				title = soup.find("h2", {"class": "fl-post-feed-title"})
 				anchor = title.nextElement
-				body = soup.find("div", {"class": "fl-post-feed-content"})
-				p = body.nextElement
-				strong = p.nextElement
-				contents = strong.toString()
-				var result = {
-					title: anchor.attrs.title,
-					body: contents
-				}
-				self.sendSocketNotification('PRAYER_RESULT', result);
+				link = anchor.attrs.href
+				request({ url: link, method: 'GET' }, function(error, response, body) {
+					if(!error && response.statusCode == 200){
+						var soup = new JSSoup(body);
+						// extract content
+						body = soup.find("div", {"class": "fl-post-content"})
+						contents = body.toString()
+						var result = {
+							title: anchor.attrs.title,
+							body: contents
+						}
+						self.sendSocketNotification('PRAYER_RESULT', result);
+					}
+				});
 			}
 		});
 	}
